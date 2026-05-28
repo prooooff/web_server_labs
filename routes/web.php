aa5547d854709fe7a90d4558697810f634bbf710
Andriy <terk0381@gmail.com>
2026-05-28 12:53:50 +0000

    lab5

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\Api\Blog\PostController;
+use App\Http\Controllers\DiggingDeeperController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,11 +18,15 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
-use App\Http\Controllers\RestTestController;
-use App\Http\Controllers\Api\Blog\PostController;
 
 Route::apiResource('rest', RestTestController::class)->names('restTest');
 
 Route::group(['prefix' => 'blog'], function () {
     Route::apiResource('posts', PostController::class)->names('blog.posts');
+});
+
+// Маршрут для тестування колекцій (DiggingDeeper)
+Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+        ->name('digging_deeper.collections');
 });
