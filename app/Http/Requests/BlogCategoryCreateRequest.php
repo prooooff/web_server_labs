2b6b5c25ee3d35f1a98935570aa4a0ae7f26a2d3
Andriy <terk0381@gmail.com>
2026-06-14 16:25:42 +0000

    lab18 not final2

--- app/Http/Requests/BlogCategoryCreateRequest.php
+++ app/Http/Requests/BlogCategoryCreateRequest.php
@@ -19,7 +19,7 @@ class BlogCategoryCreateRequest extends FormRequest
             'title' => 'required|min:5|max:200',
             'slug' => 'max:200',
             'description' => 'string|max:500|min:3',
-            'parent_id' => 'required|integer|exists:blog_categories,id',
+            'parent_id' => 'nullable|integer|exists:blog_categories,id',
         ];
     }
 }
